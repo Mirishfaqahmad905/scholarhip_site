f6260a1c82538f399fb3e0b610c5450c934b2147
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-07-16 05:38:31 -0700

    Update vite.config.js

--- frontend/webapp/vite.config.js
+++ frontend/webapp/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "./", // important for deployment
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  }
+   build: {
+    outDir: 'dist'
+  },
 });
