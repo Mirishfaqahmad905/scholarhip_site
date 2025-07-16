be2480224d4239458e0c0737b14ccd448a395556
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-07-16 05:17:02 -0700

    Update constant.jsx

--- frontend/webapp/src/constant/constant.jsx
+++ frontend/webapp/src/constant/constant.jsx
@@ -1,5 +1,5 @@
 const Api_url = {
-   BACKEND_URI: "https://scholarhip-site-backend-git-main-mir-ishfaq-ahmads-projects.vercel.app",
+   BACKEND_URI: "scholarhip-site-backend.vercel.app",
 };
 
 export default Api_url;
