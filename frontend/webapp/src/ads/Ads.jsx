ef40b599842f8374113f30cb544b22c758bff87f
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-07-17 23:42:54 +0500

    Update Ads.jsx

--- frontend/webapp/src/ads/Ads.jsx
+++ frontend/webapp/src/ads/Ads.jsx
@@ -6,10 +6,41 @@ const Ads = () => {
       <h2 className="text-lg font-bold mb-4 text-center">Sponsored Ads</h2>
       <div className="space-y-4">
         <div className="bg-white rounded shadow p-4 text-center">
-          <p className="text-sm text-gray-700">Ad Space 1</p>
+          <p className="text-sm text-gray-700">
+            
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7807051843526803"
+     crossorigin="anonymous"></script>
+<!-- Sidebar -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7807051843526803"
+     data-ad-slot="3341115609"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+            
+            1</p>
         </div>
         <div className="bg-white rounded shadow p-4 text-center">
-          <p className="text-sm text-gray-700">Ad Space 2</p>
+          <p className="text-sm text-gray-700">
+            
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7807051843526803"
+     crossorigin="anonymous"></script>
+<!-- Sidebar -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7807051843526803"
+     data-ad-slot="3341115609"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+            
+            
+            2</p>
         </div>
         <div className="bg-white rounded shadow p-4 text-center">
           <p className="text-sm text-gray-700">Ad Space 3</p>
