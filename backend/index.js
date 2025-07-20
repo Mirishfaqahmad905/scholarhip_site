95bcb6a04b6717d5e7b8450681ed6ba5015f03c5
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-07-20 09:24:19 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -102,27 +102,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ✅ Correct CORS Setup
-// app.use(cors({
-//   origin: 'https://www.scholarshipopertunity.com', // ✅ updated domain
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
-const allowedOrigins = [
-  // 'https://scholarhip-site-client.vercel.app',
-  'https://www.scholarshipopertunity.com'
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: 'https://www.scholarshipopertunity.com', // ✅ updated domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
+
+// const allowedOrigins = [
+//   // 'https://scholarhip-site-client.vercel.app',
+//   'https://www.scholarshipopertunity.com'
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
